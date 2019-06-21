eedbe81103db7415ae30c6471cbab3c7c32b921f
Lasse Nielsen <charmixer@users.noreply.github.com>
2019-06-21 13:16:10 +0200

    Consent flow in working state, but with a lot of missing features

--- config/config.go
+++ config/config.go
@@ -5,17 +5,19 @@ import (
   "strings"
 )
 
-/*
-RedirectURL:  redirect url,
-ClientID:     "GOOGLE_CLIENT_ID",
-ClientSecret: "CLIENT_SECRET",
-Scopes:       []string{"scope1", "scope2"},
-Endpoint:     oauth2 endpoint,
-*/
-
 type HydraConfig struct {
-  Url             string
-  AdminUrl        string
+  Url                           string
+  AdminUrl                      string
+}
+
+type ConsentBackendConfig struct {
+  Url                           string
+  AuthorizationsUrl             string
+  AuthorizationsAuthorizeUrl    string
+}
+
+type ConsentFrontendConfig struct {
+  CsrfAuthKey string
 }
 
 type OAuth2ClientConfig struct {
@@ -27,17 +29,25 @@ type OAuth2ClientConfig struct {
 }
 
 var Hydra HydraConfig
+var CPBe ConsentBackendConfig
+var CPFe ConsentFrontendConfig
 var OAuth2Client OAuth2ClientConfig
 
 func InitConfigurations() {
-  Hydra.Url                   = getEnvStrict("HYDRA_URL")
-  Hydra.AdminUrl              = getEnvStrict("HYDRA_ADMIN_URL")
+  Hydra.Url                             = getEnvStrict("HYDRA_URL")
+  Hydra.AdminUrl                        = getEnvStrict("HYDRA_ADMIN_URL")
 
-  OAuth2Client.ClientId       = getEnv("OAUTH2_CLIENT_CLIENT_ID")
-  OAuth2Client.ClientSecret   = getEnv("OAUTH2_CLIENT_ClIENT_SECRET")
-  OAuth2Client.Scopes         = strings.Split(getEnv("OAUTH2_CLIENT_SCOPES"), ",")
-  OAuth2Client.RedirectURL    = getEnv("OAUTH2_CLIENT_REDIRECT_URL")
-  OAuth2Client.Endpoint       = getEnv("OAUTH2_CLIENT_ENDPOINT")
+  CPBe.Url                              = getEnvStrict("CP_BACKEND_URL")
+  CPBe.AuthorizationsUrl                = CPBe.Url + "/v1/authorizations"
+  CPBe.AuthorizationsAuthorizeUrl       = CPBe.AuthorizationsUrl + "/authorize"
+
+  CPFe.CsrfAuthKey                      = getEnv("CP_FRONTEND_CSRF_AUTH_KEY")
+
+  OAuth2Client.ClientId                 = getEnv("OAUTH2_CLIENT_CLIENT_ID")
+  OAuth2Client.ClientSecret             = getEnv("OAUTH2_CLIENT_ClIENT_SECRET")
+  OAuth2Client.Scopes                   = strings.Split(getEnv("OAUTH2_CLIENT_SCOPES"), ",")
+  OAuth2Client.RedirectURL              = getEnv("OAUTH2_CLIENT_REDIRECT_URL")
+  OAuth2Client.Endpoint                 = getEnv("OAUTH2_CLIENT_ENDPOINT")
 }
 
 func getEnv(name string) string {
--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
   "github.com/gin-gonic/gin"
   "golang-cp-fe/config"
+  "golang-cp-fe/interfaces"
+  "golang-cp-fe/gateway/cpbe"
+  "github.com/gorilla/csrf"
+  "github.com/gwatts/gin-adapter"
+  "fmt"
 )
 
 type authorizeForm struct {
@@ -16,24 +21,67 @@ func init() {
 func main() {
     r := gin.Default()
 
+    // Use CSRF on all our forms.
+    fmt.Println("Using insecure CSRF for devlopment. Do not do this in production")
+    adapterCSRF := adapter.Wrap(csrf.Protect([]byte(config.CPFe.CsrfAuthKey), csrf.Secure(false)))
+    // r.Use(adapterCSRF) // Do not use this as it will make csrf tokens for public files aswell which is just extra data going over the wire, no need for that.
+
     r.Static("/public", "public")
 
     r.LoadHTMLGlob("views/*")
 
-    r.GET("/", getAuthorizeHandler)
-    r.GET("/authorize", getAuthorizeHandler)
-    r.POST("/authorize", postAuthorizeHandler)
+    r.GET("/", adapterCSRF, getAuthorizeHandler)
+    r.GET("/authorize", adapterCSRF, getAuthorizeHandler)
+    r.POST("/authorize", adapterCSRF, postAuthorizeHandler)
 
     r.Run() // defaults to :8080, uses env PORT if set
 }
 
 func getAuthorizeHandler(c *gin.Context) {
-    c.HTML(200, "authorize.html", nil)
+
+  // comes from hydra redirect
+  challenge := c.Query("consent_challenge")
+
+  cpBeAuthorizationsAuthorizeResponse, _ := cpbe.GetAuthorizationsAuthorize(challenge)
+
+  var consents = make(map[int]map[string]string)
+  for index, name := range cpBeAuthorizationsAuthorizeResponse.RequestedScopes {
+    // index is the index where we are
+    // element is the element from someSlice for where we are
+    consents[index] = map[string]string{
+      "name": name,
+    }
+  }
+
+  c.HTML(200, "authorize.html", gin.H{
+    csrf.TemplateTag: csrf.TemplateField(c.Request),
+    "requested_scopes": consents,
+    "challenge": challenge,
+  })
 }
 
 func postAuthorizeHandler(c *gin.Context) {
-    var form authorizeForm
-    c.Bind(&form)
-    c.JSON(200, gin.H{
-        "consents": form.Consents })
+  var form authorizeForm
+  c.Bind(&form)
+
+  // comes from form post url
+  challenge := c.Query("challenge")
+  cpBePostAuthorizationsAuthorizeRequest := interfaces.CPBePostAuthorizationsAuthorizeRequest{
+    Challenge: challenge,
+    GrantScopes: form.Consents,
+  }
+
+  authorizeResponse, _ := cpbe.PostAuthorizationsAuthorize(cpBePostAuthorizationsAuthorizeRequest)
+
+  fmt.Println(authorizeResponse)
+
+  if authorizeResponse.Authorized {
+    c.Redirect(302, authorizeResponse.RedirectTo)
+    c.Abort()
+    return
+  }
+
+  c.JSON(200, gin.H{
+    "authorized": false,
+  })
 }
